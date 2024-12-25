38a4f220967e86e79775c50b9a91c696b0758b6e
Nader Mohamed <Nadercs2018@gmail.com>
2024-12-25 23:13:00 +0200

    add style for Haeder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaHeart, FaShoppingBasket, FaUser, FaBars, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
@@ -7,14 +7,17 @@ import { useSelector } from 'react-redux';
 import { Navigate } from '@/utilts/Navigate';
 import Image from 'next/image';
 import NaderShop from '../public/NaderShop.png'
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const pathName = usePathname()
+
   const [isOpen, setIsOpen] = useState(false);
   const user = useSelector((state) => state.auth.user)
   const cart = useSelector((state) => state.cart.cart)
-const handlePortofilioPage = ()=>{
-  Navigate(window.location.origin,'/profile')
-}
+  const handlePortofilioPage = () => {
+    Navigate(window.location.origin, '/profile')
+  }
 
 
   return (
@@ -22,18 +25,18 @@ const handlePortofilioPage = ()=>{
       <div className="container mx-auto px-6  flex justify-between items-center">
         {/* Logo */}
         <div className="text-2xl font-bold text-gray-800 cursor-pointer">
-          <Link href="/"><Image src={NaderShop}  width={120} height={100} priority alt='NaderShop' /></Link>
+          <Link href="/"><Image src={NaderShop} width={120} height={100} priority alt='NaderShop' /></Link>
         </div>
 
         {/* Menu for large screens */}
         <nav className="hidden md:flex items-center space-x-6">
-          <Link href="/" className="text-gray-700 hover:text-gray-900">
+          <Link href="/" className={`${pathName == '/' ? 'text-gray-950 border-b-2' : 'text-gray-700'} hover:text-gray-900`}>
             Home
           </Link>
-          <Link href="/product" className="text-gray-700 hover:text-gray-900">
+          <Link href="/product" className={`${pathName == '/product' ? 'text-gray-950 border-b-2' : 'text-gray-700'} hover:text-gray-900`}>
             Product
           </Link>
-          <Link href="/contactus" className="text-gray-700 hover:text-gray-900">
+          <Link href="/contactus" className={`${pathName == '/contactus' ? 'text-gray-950 border-b-2' : 'text-gray-700'} hover:text-gray-900`}>
             Contact Us
           </Link>
         </nav>
@@ -47,7 +50,7 @@ const handlePortofilioPage = ()=>{
               <div className="basket relative px-1 py-2" >
                 <FaShoppingBasket className="text-[1.3rem] text-gray-600 hover:text-gray-800 cursor-pointer" />
                 <span className='w-[1.1rem] h-[1.1rem] rounded-full bg-red-600 text-white 
-              font-semibold top-[-1%] right-[-1%] absolute text-center align-middle'>{user?.email?cart.length:0}</span>
+              font-semibold top-[-1%] right-[-1%] absolute text-center align-middle'>{user?.email ? cart.length : 0}</span>
               </div>
 
             </Link>
